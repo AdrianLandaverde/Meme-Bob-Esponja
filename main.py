3895bc820af9d496796658a6c77739e8472455c3
AdrianLandaverde <86375510+AdrianLandaverde@users.noreply.github.com>
2023-10-30 20:47:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 from fastapi import FastAPI
+import vertexai
+from vertexai.language_models import TextGenerationModel
 
 app = FastAPI()
 
 @app.get("/")
 def hello_world():
+    return {'message':'Hello World'}
+
+@app.get("/gcloud")
+def hello_world():
+    vertexai.init(project="hip-principle-403314", location="us-central1")
+    parameters = {
+        "candidate_count": 1,
+        "max_output_tokens": 1024,
+        "temperature": 0.2,
+        "top_p": 0.8,
+        "top_k": 40
+    }
+    model = TextGenerationModel.from_pretrained("text-bison")
+    response = model.predict(
+        """What are the advantages of using google cloud for AI models""",
+        **parameters
+    )
+    print(f"Response from Model: {response.text}")
     return {'message':'Hello World'}
 
 @app.get("/{frase}")
